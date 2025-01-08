36fc426494cf405aff5a71f6d2f8332b62122496
Joaquin Thiogo <joanthio09@gmail.com>
2025-01-08 16:30:12 +0700

    awdadawdwada

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       fontFamily: {
         quantico: ["Quantico", "serif"],
         jersey: ["'Jersey 10'", "serif"],
+        jersey25: ["'Jersey 25'", "serif"],
         roboto: ["Roboto", "serif"],
+        khdam: ["'Kdam Thmor Pro'", "serif"],
+        inter: ["'Inter Tight'", "serif"],
+        montserrat: ["Montserrat", "serif"],
       },
     },
   },
